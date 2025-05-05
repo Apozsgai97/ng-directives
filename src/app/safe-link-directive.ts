8bf73479f7f341e6b3850e4e426d185860e8751b
Adrienn Pozsgai <apozsgai97@gmail.com>
2025-05-05 11:22:42 +0200

    Enhance SafeLinkDirective to include query parameters for navigation links in LearningResourcesComponent

--- src/app/safe-link-directive.ts
+++ src/app/safe-link-directive.ts
@@ -1,4 +1,4 @@
-import { Directive } from '@angular/core';
+import { Directive, input } from '@angular/core';
 
 @Directive({
   selector: 'a[appSafeLink]',
@@ -8,6 +8,8 @@ import { Directive } from '@angular/core';
   },
 })
 export class SafeLinkDirective {
+ queryParam = input('myapp');
+ 
   constructor() {
     console.log('SafeLinkDirective initialized');
   }
@@ -17,6 +19,10 @@ export class SafeLinkDirective {
       'You are about to leave this page. Do you want to continue?'
     );
     if (wantsToLeave) {
+      const address = (event.target as HTMLAnchorElement).href;
+
+      (event.target as HTMLAnchorElement).href = address + '?from=' + this.queryParam();
+
       return;
     }
     event.preventDefault();
